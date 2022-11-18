cf6fb89d889e25767f51f3b441e1e0c7b8031516
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:29:14 -0300

    Create PagarFatura.py
    // Programa que cria uma apresentação de fatura para pagamento.

---
+++ PagarFatura.py
@@ -0,0 +1,12 @@
+// Programa em Python:
+// Programa que cria uma apresentação de fatura para pagamento.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+nomecliente = input ("Digite o nome do cliente: ")
+diavencimento = input ("Digite o dia de vencimento: ")
+mesvencimento = input ("Digite o mês de vencimento: ")
+valorfatura = input ("Digite o valor da fatura: ")
+
+print ("Olá, ", nomecliente)
+print ("A Sua fatura com vencimento em {} de {} no valor de R$ {} está fechada.".format(diavencimento, mesvencimento, valorfatura))
+
