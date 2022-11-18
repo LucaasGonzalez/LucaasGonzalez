03f1a05b31766e1af5b59958a585951fb5e28cae
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:24:30 -0300

    Create Multiplicação.py
    // Programa em Python:
    // Programa que faz uma multiplicação.
    // Se te ajudei com o código, me siga no Instagram @xluucas.

---
+++ Multiplicação.py
@@ -0,0 +1,12 @@
+// Programa em Python:
+// Programa que faz uma multiplicação.
+// Se te ajudei com o código, me siga no Instagram @xluucas.
+
+print ("Exercício 4")
+
+a = 3
+b = 5
+
+conta = (2 * a) * (3 * b)
+
+print ("O resposta desse exercício é: ", conta)
