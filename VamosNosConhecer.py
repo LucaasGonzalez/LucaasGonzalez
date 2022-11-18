ec59b064b7cdab0398aedf5a67bfa394f3b2c2f0
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:19:26 -0300

    Create VamosNosConhecer.py

---
+++ VamosNosConhecer.py
@@ -0,0 +1,11 @@
+// Programa em Python:
+// Programa para nos conhecermos melhor.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+print ("Vamos te conhecer!!!")
+
+a = input ("Digite seu nome completo:")
+
+print ("Seu nome é: ", a)
+
+print("Foi um prazer conhecer você!!!")
