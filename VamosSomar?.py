ffb283c0d0f80af219ad168bb418fa74bee7e61b
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:51:34 -0300

    Create VamosSomar?.py
    Programa que soma dois valores.

---
+++ VamosSomar?.py
@@ -0,0 +1,12 @@
+print ("VAMOS SOMAR!!!")
+
+a = int (input("Digite o 1º número que deseja somar:"))
+b = int (input("Digite o 2º número que deseja somar:"))
+
+soma = a + b
+
+print ("A soma dos numeros é:", soma)
+
+print ("VOLTE SEMPRE!!!")
+
+
