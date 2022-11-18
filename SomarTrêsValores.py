66117a59c5c5df0db281956097a4c76bfafa9f2b
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:25:51 -0300

    Create SomarTrêsValores.py
    Programa que soma três valores.

---
+++ SomarTrêsValores.py
@@ -0,0 +1,15 @@
+// Programa em Python:
+// Programa que soma três valores.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+print ("Programa para somar três valores!!!")
+
+a = int (input ("Digite o primeiro valor: "))
+
+b = int (input ("Digite o segundo valor: "))
+
+c = int (input ("Digite o terceiro valor:"))
+
+d = a + b + c
+
+print ("O valor total da soma dos três valores são: ", d)
