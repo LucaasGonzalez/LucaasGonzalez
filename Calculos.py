e94bb3f7a2265d1af7afcfc9c60dc3c0d43322bc
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:30:45 -0300

    Create Calculos.py
    // Programa que faz três tipos de calculos.

---
+++ Calculos.py
@@ -0,0 +1,22 @@
+// Programa em Python:
+// Programa que faz três tipos de calculos.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+a = 10 + 20 * 30
+
+print("Exercício A")
+print ("10 + 20 * 30")
+print ("A resposta do exercicio A é: ", a)
+
+
+b = 4 ** 2 / 30
+
+print("Exercício B")
+print ("(4² / 30)")
+print ("A resposta do exercício B é: ", b)
+
+c = (9 ** 4 + 2) * 6 - 1
+
+print("Exercício C")
+print ("(9 ** 4 + 2) * 6 - 1")
+print ("A resposta do exercício C é:", c)
