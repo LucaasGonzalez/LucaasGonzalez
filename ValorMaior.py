cc85d22cc680eb7d694f2ab8c8799ce70cb6a4c7
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:45:47 -0300

    Create ValorMaior.py
    Programa que mostra o maior, menor ou iguais números digitados

---
+++ ValorMaior.py
@@ -0,0 +1,19 @@
+// Programa em Python:
+// Programa que mostra o maior, menor ou iguais números digitados.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+a = int(input("Entre com o prímeiro valor: "))
+
+b = int(input("Entre com o segundo valor: "))
+
+if a > b:
+    
+    print ("O prímeiro valor é maior.")
+    
+if b > a:
+    
+    print ("O segundo valor é maior.")
+    
+if a == b:
+    
+    print ("Os números são iguais.")
