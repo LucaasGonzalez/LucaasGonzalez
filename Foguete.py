ee69637d0fe61bdbac6d6e88972a94d5f2fde525
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:33:15 -0300

    Create Foguete.py
    Programa que simula um disparo de um foguete com contagem regressiva.

---
+++ Foguete.py
@@ -0,0 +1,13 @@
+// Programa em Python:
+// Programa que simula um disparo de um foguete com contagem regressiva.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+from time import sleep
+
+for cont in range(10, -1, -1):
+
+    print(cont)
+
+    sleep(1)
+
+print("Fogo!")
