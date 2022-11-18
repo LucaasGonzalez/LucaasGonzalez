c44b1112d0c0579cac460398c97e48489b451dac
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:35:50 -0300

    Create FunçãoParte2.py
    Exemplo de criação de funções parte 2.

---
+++ FunçãoParte2.py
@@ -0,0 +1,16 @@
+def maximo (a, b):
+    
+    if a >= b:
+
+        return a
+
+    else:
+
+        return b
+
+x = int ( input ("digite um número: "))
+
+y = int ( input ("digite outro número: "))
+
+print ("O número maior é: ", maximo(x , y))
+
