a3ec87405056610cb70e8eaec410aa081fbc2b05
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:27:44 -0300

    Create CalcularAumento.py
    // Programa que calcula o aumento salarial de um funcionário.

---
+++ CalcularAumento.py
@@ -0,0 +1,12 @@
+// Programa em Python:
+// Programa que calcula o aumento salarial de um funcionário.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+print ("Programa para calcular aumentos!!!")
+
+salario = int ( input ("Digite o valor do seu salário: "))
+
+a = (salario * 15 / 100) + salario
+
+print ("O novo valor do seu salário é: ", a)
+
