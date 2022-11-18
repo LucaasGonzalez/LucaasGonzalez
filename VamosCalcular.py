f4679704845691d73744521b3e4aa218d8773434
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:21:59 -0300

    Create VamosCalcular.py

---
+++ VamosCalcular.py
@@ -0,0 +1,11 @@
+// Programa em Python:
+// Programa que calcula uma fórmula.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+print ("VAMOS CALCULAR!!!")
+
+print ("O Exercício é: (((10 % 3) * (10^2)) + 1) - ((10 * 4) / 2)")
+
+a = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+
+print ("A resposta é: ", a)
