575e1da2881992ff036fa7758105ca7b984590c8
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:52:31 -0300

    Create String2.py
    String parte 2

---
+++ String2.py
@@ -0,0 +1,14 @@
+nome = 'ecila'
+nome[::-1]
+
+var = "Parabéns para você:"
+var.count('a')
+
+var2 = "Parabéns para você!"
+var2.strip()
+
+var.split (' ')
+
+var.raplace ('e','a')
+
+var.center(50)
