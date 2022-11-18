25100ca689166ef5323e54d072f1e8a193b2c49b
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:47:01 -0300

    Create CarroNovoOuVelho.py
    // Programa para dizer se seu carro é novo ou velho.

---
+++ CarroNovoOuVelho.py
@@ -0,0 +1,13 @@
+// Programa em Python:
+// Programa para dizer se seu carro é novo ou velho.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+idade = int(input("Digite a idade do seu carro:"))
+
+if idade <=3:
+
+    print ("Seu carro é novo.")
+    
+else:
+
+     print ("Seu carro é velho.")
