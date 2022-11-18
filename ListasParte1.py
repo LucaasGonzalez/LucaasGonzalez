d16f633333ade74da7a6600b6b551600628b0c89
Lucas Gonzalez <118545975+LucaasGonzalez@users.noreply.github.com>
2022-11-18 12:42:04 -0300

    Create ListasParte1.py
    // Exemplo de Listas em Python.

---
+++ ListasParte1.py
@@ -0,0 +1,19 @@
+// Programa em Python:
+// Exemplo de Listas em Python.
+// Se te ajudei com o código, me siga no instagram @xluucas.
+
+
+
+primos = [2, 3, 5, 7, 11]
+
+primos.append(13)
+
+indice = 0
+
+while indice < len (primos):
+
+    print ("elemento de indice %d = %d" % (indice, primos [indice]))
+
+    indice += 1
+
+
